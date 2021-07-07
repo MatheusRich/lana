14de0582be9d35009abe7d2ac82c1b43551dc3d2
Matheus Richard <matheusrichardt@gmail.com>
2021-07-07 00:44:32 -0300

    Add more tests to parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -154,6 +154,56 @@ mod tests {
     }
 
     #[test]
+    fn it_parses_strings() {
+        let input = vec![Token::new(
+            TokenKind::String("hello world".into()),
+            SrcLocation::new(1, 11),
+        )];
+
+        let (result, _) = parse(&input).expect("Could not parse string");
+
+        assert_eq!(LanaExpr::String("hello world".into()), result);
+    }
+
+    #[test]
+    fn it_parses_an_empty_list() {
+        let input = vec![
+            Token::new(TokenKind::LParen, SrcLocation::new(1, 1)),
+            Token::new(TokenKind::RParen, SrcLocation::new(1, 2)),
+        ];
+
+        let result = parse_all(&input).expect("Could not parse empty list");
+
+        assert_eq!(vec![LanaExpr::List(vec![])], result);
+    }
+
+    #[test]
+    fn it_parses_a_list() {
+        let input = vec![
+            Token::new(TokenKind::LParen, SrcLocation::new(1, 1)),
+            Token::new(TokenKind::Number(1.0), SrcLocation::new(1, 2)),
+            Token::new(TokenKind::RParen, SrcLocation::new(1, 3)),
+        ];
+
+        let result = parse_all(&input).expect("Could not parse a list");
+
+        assert_eq!(vec![LanaExpr::List(vec![LanaExpr::Number(1.0)])], result);
+    }
+
+    #[test]
+    fn it_errors_on_unterminated_lists() {
+        let opening_paren = Token::new(TokenKind::LParen, SrcLocation::new(1, 1));
+        let input = vec![
+            opening_paren.clone(),
+            Token::new(TokenKind::Number(1.0), SrcLocation::new(1, 2)),
+        ];
+
+        let result = parse_all(&input).expect_err("Didn't fail on unterminated list");
+
+        assert_eq!(LanaErr::UnterminatedExpr((')', opening_paren)), result);
+    }
+
+    #[test]
     fn it_errors_on_unterminated_strings() {
         let unterminated_string = Token::new(
             TokenKind::UnterminatedString("\"unterminated".to_string()),
@@ -161,7 +211,7 @@ mod tests {
         );
         let input = vec![unterminated_string.clone()];
 
-        let result = parse_all(&input).expect_err("Didn't failed on unterminated string");
+        let result = parse_all(&input).expect_err("Didn't fail on unterminated string");
 
         assert_eq!(
             LanaErr::UnterminatedExpr(('"', unterminated_string)),
@@ -179,5 +229,13 @@ mod tests {
             LanaErr::UnexpectedToken(Token::new(TokenKind::RParen, SrcLocation::new(1, 1))),
             result
         );
+    }
+
+    #[test]
+    #[should_panic(expected = "Cannot parse atom from token ')' at line 1, column 1")]
+    fn it_panics_on_invalid_atoms() {
+        let input = Token::new(TokenKind::RParen, SrcLocation::new(1, 1));
+
+        parse_atom(&input).expect_err("Didn't panic on unexpected token");
     }
 }
